a46c3f9143aa6c0a63123b532e0fb2a2eaee96af
Joris Kamminga <joris.kamminga@gmail.com>
2026-04-19 19:22:00 +0200

    Updated nav sidebar

--- src/components/navigation/SwNavGroup.vue
+++ src/components/navigation/SwNavGroup.vue
@@ -9,8 +9,7 @@ const sidebarOpen = inject<Ref<boolean>>('sidebarOpen', ref(true));
 </script>
 
 <template>
-  <div v-if="!sidebarOpen" class="sw-nav-group__divider" />
-  <div class="sw-nav-group" :class="{ 'sw-nav-group--collapsed': !sidebarOpen }">
+  <div class="sw-nav-group">
     <span v-if="sidebarOpen" class="sw-nav-group__label">{{ label }}</span>
     <slot />
   </div>
@@ -23,14 +22,6 @@ const sidebarOpen = inject<Ref<boolean>>('sidebarOpen', ref(true));
   @apply flex flex-col gap-0.5 pt-4 first:pt-2;
 }
 
-.sw-nav-group--collapsed {
-  @apply pt-2 pb-2;
-}
-
-.sw-nav-group__divider {
-  @apply h-0.5;
-  @apply bg-border rounded-2xl;
-}
 
 .sw-nav-group__label {
   @apply px-2 pb-1 text-xs font-semibold uppercase text-text-muted select-none tracking-wider;
